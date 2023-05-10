d7ca21492349b93c714c20c1bf9e0b3b292de379
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2023-05-10 20:33:31 +0530

    Added solution - LeetHub

---
+++ K-th missing element - GFG/kth-missing-element.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int KthMissingElement(int a[], int n, int k);
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        
+        cout << KthMissingElement(a, n, k) << endl;
+    
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+int KthMissingElement(int arr[], int n, int k) {
+    int count = 0;
+
+    for (int i = 1; i < n; i++) {
+        int diff = arr[i] - arr[i - 1] - 1;  // Calculate the difference between adjacent elements
+        count += diff;
+
+        if (count >= k) {
+            // Calculate the Kth missing element
+            return arr[i - 1] + (k - (count - diff));
+        }
+    }
+
+    // If the loop completes without finding the Kth missing element
+    return -1;
+}
