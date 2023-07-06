61082647dc32ba2c0964fcbdebff2b7e4eb12d54
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2023-07-07 00:34:55 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.cpp
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -3,26 +3,61 @@
 using namespace std;
 
 // } Driver Code Ends
-class Solution{
-    public:
-    
-    int longestCommonSubstr (string S1, string S2, int n, int m)
+
+
+class Solution
+{
+public:
+    int longestCommonSubstr(string S1, string S2, int n, int m)
     {
         // your code here
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        return optimized(S1, S2, n, m);
+        return tabulation(S1, S2, n, m);
+    }
+
+private:
+    int tabulation(string S1, string S2, int n, int m)
+    {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
         int ans = 0;
-        for(int i=1;i<=n;i++)
-            for(int j=1;j<=m;j++)
-                if(S1[i-1]==S2[j-1])
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++)
+                if (S1[i - 1] == S2[j - 1])
                 {
-                    dp[i][j] = 1+dp[i-1][j-1];
-                    ans = max(ans,dp[i][j]);
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    ans = max(ans, dp[i][j]);
                 }
                 else
                     dp[i][j] = 0;
         return ans;
     }
+    int optimized(string S1, string S2, int n, int m)
+    {
+        vector<int> prev(max(n, m) + 1, 0);
+        vector<int> curr(max(n, m) + 1, 0);
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (S1[i - 1] == S2[j - 1])
+                {
+                    curr[j] = 1 + prev[j - 1];
+                    ans = max(ans, curr[j]);
+                }
+                else
+                {
+                    curr[j] = 0;
+                }
+            }
+            prev = curr;
+        }
+        return ans;
+    }
 };
+
+
+
 
 //{ Driver Code Starts.
 
